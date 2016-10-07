d80d90b4ec0de8f7e81c4195f16e89ef1966bc79
Nico <nicolasgargano@gmail.com>
2016-10-07 11:47:48 -0300

    Added CategoriesCatalogue constructor.

--- src/rUBERn/CategoriesCatalogue.java
+++ src/rUBERn/CategoriesCatalogue.java
@@ -2,8 +2,14 @@ package rUBERn;
 
 // Created by nico on 10/1/16.
 
+import java.util.HashMap;
+
 public class CategoriesCatalogue {
-    private java.util.HashMap catalogue;
+    private HashMap catalogue;
+
+    public CategoriesCatalogue(HashMap catalogue) {
+        this.catalogue = catalogue;
+    }
 
     public float evaluateCarImageCost(Car car) {
         return 0;

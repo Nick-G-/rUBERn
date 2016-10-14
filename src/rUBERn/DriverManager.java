18974732aea0977f1bd9ed082fbae3973c4de11a
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2016-10-14 12:34:37 -0300

    driver Manager methods

--- src/rUBERn/DriverManager.java
+++ src/rUBERn/DriverManager.java
@@ -15,7 +15,24 @@ public class DriverManager {
 
     private ArrayList<Driver> rankDrivers(Journey journey) {
         ArrayList<Driver> rankedDrivers = null;
+        ArrayList<Driver> possibleDrivers = drivers;
+        for (int i=0; i<drivers.size(); i++){
+            Driver bestDriver = bestDriver(journey.getOrigin(),possibleDrivers);
+            rankedDrivers.add(bestDriver);
+            possibleDrivers.remove(possibleDrivers.indexOf(bestDriver));
+        }
+
         return rankedDrivers;
+    }
+
+    private Driver bestDriver(Location origin, ArrayList<Driver> driversList){
+        Driver bestDriver = drivers.get(0);
+        for (int i=0; i<driversList.size(); i++){
+            if(bestDriver.getLocation().distanceTo(origin)>drivers.get(i).getLocation().distanceTo(origin)){
+                bestDriver = drivers.get(i);
+            }
+        }
+        return bestDriver;
     }
 
     public void findDriverForJob(Job job) {

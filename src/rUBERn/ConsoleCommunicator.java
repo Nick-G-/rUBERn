978d52f06c57dc050c28cc95b07008b7ec305c69
facundo <facundo915@gmail.com>
2016-10-14 12:32:41 +0000

    Updated ConsoleComunicator's Categories

--- src/rUBERn/ConsoleCommunicator.java
+++ src/rUBERn/ConsoleCommunicator.java
@@ -14,8 +14,8 @@ public class ConsoleCommunicator {
         Scanner scanner = new Scanner(System.in);
         while (on) {
             System.out.println("Menu: ");
-            System.out.println("1. Dar de alta chofer");
-            System.out.println("2. Dar de alta cliente");
+            System.out.println("1. Agregar chofer");
+            System.out.println("2. Agergar cliente");
             System.out.println("3. Alterar estado de los choferes");
             System.out.println("4. Pedir viaje como cliente");
             System.out.println("5. Salir");
@@ -48,8 +48,30 @@ public class ConsoleCommunicator {
         System.out.println("Ingrese el nombre del chofer: ");
         String name = scanner.nextLine();
         System.out.println("Ingrese los datos del auto del chofer:");
+        boolean hasSelectedCategory = false;
+        String category = "Normal";
+        while (!hasSelectedCategory){
         System.out.println("Categoria: ");
-        String category = scanner.nextLine();
+        System.out.println("1. Deluxe");
+        System.out.println("2. Normal");
+        System.out.println("3. Berreta");
+            switch (scanner.nextInt()) {
+               case 1:
+                    category = "Deluxe";
+                    hasSelectedCategory = true;
+                    break;
+                case 2:
+                 category = "Normal";
+                 hasSelectedCategory = true;
+                 break;
+                case 3:
+                 category = "Berreta";
+                 hasSelectedCategory = true;
+                 break;
+                default:
+                 System.out.println("Opcion invalida");
+            }
+        }
         System.out.println("Ingrese la capacidad del auto: ");
         int capacity = scanner.nextInt();
         System.out.println("Ingrese la ubicacion del chofer x:");

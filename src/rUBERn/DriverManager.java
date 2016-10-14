7586e31f8658c449f6a9a5a75ab0389799fb3ffb
arimirra <arimirra@hotmail.com>
2016-10-14 20:13:06 -0300

    changed driver maganer methods to use imageCalculator

--- src/rUBERn/DriverManager.java
+++ src/rUBERn/DriverManager.java
@@ -17,19 +17,18 @@ public class DriverManager {
         ArrayList<Driver> rankedDrivers = null;
         ArrayList<Driver> possibleDrivers = drivers;
         for (int i=0; i<drivers.size(); i++){
-            Driver bestDriver = bestDriver(journey.getOrigin(),possibleDrivers);
+            Driver bestDriver = bestDriver(journey,possibleDrivers);
             rankedDrivers.add(bestDriver);
             possibleDrivers.remove(possibleDrivers.indexOf(bestDriver));
         }
-
         return rankedDrivers;
     }
 
-    private Driver bestDriver(Location origin, ArrayList<Driver> driversList){
-        Driver bestDriver = drivers.get(0);
+    private Driver bestDriver(Journey journey, ArrayList<Driver> driversList){
+        Driver bestDriver = driversList.get(0);
         for (int i=0; i<driversList.size(); i++){
-            if(bestDriver.getLocation().distanceTo(origin)>drivers.get(i).getLocation().distanceTo(origin)){
-                bestDriver = drivers.get(i);
+            if(imageCalculator.calculateImageCost(bestDriver,journey)>imageCalculator.calculateImageCost(driversList.get(i),journey)){
+                bestDriver = driversList.get(i);
             }
         }
         return bestDriver;

808cd7804322452f52bd19cfaf11bb179373a256
facundo <facundo915@gmail.com>
2016-10-14 11:52:29 +0000

    Updated drivermanager to use only one list of drivers, added constructor and created addDriver method

--- src/rUBERn/DriverManager.java
+++ src/rUBERn/DriverManager.java
@@ -5,9 +5,13 @@ package rUBERn;
 import java.util.ArrayList;
 
 public class DriverManager {
-    ArrayList<Driver> driversOnline = new ArrayList<>();
-    private ArrayList<Driver> drivers;
+    private ArrayList<Driver> drivers = new ArrayList<>();
     private ImageCalculator imageCalculator;
+
+    public DriverManager(){
+        this.imageCalculator = new ImageCalculator();
+        drivers = new ArrayList<>();
+    }
 
     private ArrayList<Driver> rankDrivers(Journey journey) {
         ArrayList<Driver> rankedDrivers = null;
@@ -16,6 +20,10 @@ public class DriverManager {
 
     public void findDriverForJob(Job job) {
 
+    }
+    public void addDriver(Driver... drivers){
+        for (int i=0;i<drivers.length ;i++)
+        this.drivers.add(drivers[i]);
     }
 
     private void offerJobToDriver(Job job, Driver driver) {

574bca512e858e4d0d203483028b61b47df920eb
Nico <nicolasgargano@gmail.com>
2016-10-17 10:55:51 -0300

    Contructors, move method and its test for Person.

--- src/rUBERn/Person.java
+++ src/rUBERn/Person.java
@@ -9,19 +9,32 @@ public abstract class Person implements Locatable {
 
     public Person(String name) {
         this.creditCard = new CreditCard();
-
+        this.currentLocation = new Location();
+        this.name = name;
+    }
+    public Person(String name, Location startingPoint) {
+        this.creditCard = new CreditCard();
+        this.currentLocation = new Location(startingPoint);
+        this.name = name;
     }
     public Person(CreditCard creditCard, Location startingPoint, String name) {
         this.creditCard = creditCard;
-        this.currentLocation = startingPoint;
+        this.currentLocation = new Location(startingPoint);
         this.name = name;
     }
 
-    public Location getLocation() {
-        return currentLocation;
+    public void move(double angle, double distance) {
+        currentLocation.moveX((long) (Math.cos(angle)*distance));
+        currentLocation.moveY((long) (Math.sin(angle)*distance));
     }
 
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
     public int getCreditCardNumber() {
         return creditCard.getId();
+    }
+    public String getName() {
+        return this.name;
     }
 }
---
+++ tests/rUBERn/PersonTest.java
@@ -0,0 +1,35 @@
+package rUBERn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+// Created by nico on 10/17/16.
+
+public class PersonTest {
+    @Test
+    public void move() throws Exception {
+        double tau = Math.PI*2;
+
+        Location origin = new Location(0,0);
+        Driver dan = new Driver("Dan", origin);
+                                            // starts in origin
+        dan.move(0, 5);                     // move 5m right
+        assertLocatableIsAt(dan, 5, 0);     // should be at (5,0)
+
+        dan.move(tau/4, 5);                 // move 5m up
+        assertLocatableIsAt(dan, 5, 5);     // should be at (5,5)
+
+        dan.move(dan.getCurrentLocation().angleTo(origin), dan.getCurrentLocation().distanceTo(origin)); // move to origin
+        assertLocatableIsAt(dan, 0, 0); // should be at (0,0)
+    }
+    private void assertLocatableIsAt(Locatable locatable, long coordX, long coordY) {
+        long locatableCurrentX = locatable.getCurrentLocation().getX();
+        long locatableCurrentY = locatable.getCurrentLocation().getY();
+
+        assertEquals(coordX, locatableCurrentX, 0.1);
+        assertEquals(coordY, locatableCurrentY, 0.1);
+    }
+
+}

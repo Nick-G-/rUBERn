a638b458fcdd5fb9e3a74f7f59d1110c5aac84a6
Nico <nicolasgargano@gmail.com>
2016-10-17 10:46:38 -0300

    Location changes. Added a constructor from nothing, and one from location. Added angleTo method. Added toString method. Added move and set methods for coordinates.

--- src/rUBERn/Location.java
+++ src/rUBERn/Location.java
@@ -1,11 +1,22 @@
 package rUBERn;
 
 // Created by nico on 9/30/16.
-
+import java.time.*;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Location {
     private long x;
     private long y;
+
+    public Location() {
+        this.x = ThreadLocalRandom.current().nextLong(1000);
+        this.y = ThreadLocalRandom.current().nextLong(1000);
+    }
+    public Location(Location location) {
+        this.x = location.getX();
+        this.y = location.getY();
+    }
     public Location(long x, long y){
         this.x = x;
         this.y = y;
@@ -13,12 +24,37 @@ public class Location {
     public double distanceTo(Location otherLocation) {
         return applyPythagoras(this, otherLocation);
     }
+    public double angleTo(Location otherLocation) {
+        long adj = otherLocation.x - this.x;
+        long opp = otherLocation.y - this.y;
+
+        double hyp = distanceTo(otherLocation);
+
+        double angleAbsolute = Math.acos(adj/hyp);
+        return opp>0? angleAbsolute : -angleAbsolute;
+    }
 
     private double applyPythagoras(Location a, Location b) {
         return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
     }
 
-    public void ETATo() {
+    public String toString() {
+        return "(" + x + ", " + y + ")";
     }
 
+    public void moveX(long x) {
+        this.x += x;
+    }
+
+    public void moveY(long y) {
+        this.y += y;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public long getY() {
+        return y;
+    }
 }

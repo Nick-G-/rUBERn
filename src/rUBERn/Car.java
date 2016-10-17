979ff5f3658431479831e26bced3d518c609404a
Nico <nicolasgargano@gmail.com>
2016-10-17 10:40:46 -0300

    Added speed field in Car.

--- src/rUBERn/Car.java
+++ src/rUBERn/Car.java
@@ -3,15 +3,18 @@ package rUBERn;// Created by nico on 9/30/16.
 public class Car {
     private int passengerCapacity;
     private String category;
+    private double speed;                   // meters per second, by default 5.5 (20 km/h).
 
     public Car() {
         this.passengerCapacity = 3;
-        this. category = "Normal";
+        this.category = "Normal";
+        this.speed = 5.5;
     }
 
     public Car(int passengerCapacity, String category) {
         this.passengerCapacity = passengerCapacity;
         this.category = category;
+        this.speed = 5.5;
     }
 
     public int getPassengerCapacity() {
@@ -20,5 +23,8 @@ public class Car {
 
     public String getCategory() {
         return category;
+    }
+    public double getSpeed(){
+        return speed;
     }
 }

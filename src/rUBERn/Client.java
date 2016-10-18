9945e5f9d500c2a26b43e63b77a91e41a5429108
facundo <facundo915@gmail.com>
2016-10-18 16:30:10 +0000

    Updated Client to have a boolean to check if he's wating for a journey, added methods to use this boolean.

--- src/rUBERn/Client.java
+++ src/rUBERn/Client.java
@@ -3,9 +3,11 @@ package rUBERn;// Created by nico on 9/30/16.
 import java.util.ArrayList;
 
 public class Client extends Person {
-    
+    private boolean pendingJourney;
+
     public Client(String name) {
         super(name);
+        pendingJourney = false;
     }
     public Client(CreditCard creditCard, Location startingPoint, String name) {
         super(creditCard, startingPoint, name);
@@ -19,8 +21,14 @@ public class Client extends Person {
     }
 
     public void waitForDriver() {
+    pendingJourney = true;
     }
-
+    public boolean isWating(){
+    return pendingJourney;
+    }
+    public void arrived(){
+        pendingJourney = false;
+    }
     public void leave() {
     }
 }

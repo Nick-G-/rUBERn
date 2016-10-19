f30eb8d44c2a2dd1a993dc32b21c4014d81d3990
facundo <facundo915@gmail.com>
2016-10-19 19:13:17 +0000

    Added Main class

---
+++ src/rUBERn/Main.java
@@ -0,0 +1,26 @@
+package rUBERn;
+
+import Utils.Scanner;
+
+/**
+ * Created by facundo on 10/19/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        rUBERn rubern = new rUBERn();
+        Scanner scanner = new Scanner();
+        System.out.println("Start GUI/Console");
+        System.out.println("1. GUI");
+        System.out.println("2. Console");
+        int option = scanner.nextInt();
+        if (option == 1){
+            //Something along the lines of GUICommunicator gui = new GUICommunicator();
+           //TODO: Implement GUI
+        }else if (option == 2){
+            ConsoleCommunicator console = new ConsoleCommunicator(rubern);
+            console.consoleApp();
+        }else{ System.out.println("Opcion Invalida");
+        main(null);
+        }
+    }
+}

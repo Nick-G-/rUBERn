829b64531d25a1a8ea6401461c7777c3a174c433
facundo <facundo915@gmail.com>
2016-10-21 09:31:11 +0000

    Added status classes

---
+++ src/rUBERn/Status/Online.java
@@ -0,0 +1,7 @@
+package rUBERn.Status;
+
+/**
+ * Created by facundo on 10/21/16.
+ */
+public class Online implements Status {
+}
---
+++ src/rUBERn/Status/Status.java
@@ -0,0 +1,10 @@
+package rUBERn.Status;
+
+import rUBERn.rUBERn;
+
+/**
+ * Created by facundo on 10/21/16.
+ */
+public interface Status {
+    void changeStatus(Status s);
+}
